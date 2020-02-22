faa4f075c2ce7443e6509ed6ce60dd541ae54725
Nnamdi <nnamdinwchkw@yahoo.com>
2020-02-22 16:04:24 -0600

    WIP - resolved dependencies error

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -1,1 +1,14 @@
-console.log("Testing babel/react compiler...");
+"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var greetings = <h1>Hello World</h1>;
+
+ReactDOM.render(greetings, document.getElementById('root'));
+
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './App';
+
+ReactDOM.render(<Main />, document.getElementById('root'));*/
