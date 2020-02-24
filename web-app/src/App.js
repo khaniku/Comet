37c411ab5bd8f20e4bac3bd52c6eb44b74a5710d
Nnamdi <nnamdinwchkw@yahoo.com>
2020-02-24 00:07:58 -0600

    WIP - refactored folder structure

---
+++ web-app/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="hello">
+        <h1>Testing</h1>
+      </div>
+      // <div id="login">
+      //       <div id="header">
+      //           <header>
+      //               <h2 className="right-shift">Existing User</h2>
+      //               <h3 className="right-shift">Required fields:<span> *</span></h3>
+      //           </header>
+      //       </div>
+      //       <p className="right-shift">Log into your account:</p>
+      //       <hr>
+
+      //       <form action="" method="post" id='login-form'>
+      //           <label for="email" className="right-shift">Email: </label>
+      //           <input type="email" id="email" required><span id="email-required"> *</span>
+
+      //           <label for="password" className="right-shift">Password: </label>
+      //           <input type="password" id="password" required><span id="password-required"> *</span>
+
+                
+      //           <input type="checkbox" id="toggle">
+      //           <label for="toggle" id="checkbox">Keep me logged in</label>
+
+      //           <button id="login-button">Login</button>
+      //       </form>
+      //       <hr>
+      //       <p><a href="" alt="Password reset page" className="right-shift">Forgot your password?</a></p>
+      //   </div>
+
+      //   <div id="sign-up">
+      //       <form action="" method="post">
+      //           <header>
+      //               <h2 className="right-shift">New User</h2>
+      //           </header>
+      //           <section>
+      //               <p className="right-shift">Create new user account:</p>
+      //               <button id="signup-button" className="right-shift">Sign up</button>
+      //           </section>
+      //       </form>
+      //   </div>
+    );
+  }
+}
+
+export default App;
