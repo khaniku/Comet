aaa707d1c28fc9366132841d6e2f4238aec8ff56
Nnamdi <nnamdinwchkw@yahoo.com>
2020-02-26 20:51:38 -0600

    Refactored branch

---
+++ web-app/js/App.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*import React, {Component} from 'react';
+
+class Main extends Component {
+  render() {
+    return(
+      <div className="root">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;*/
---
+++ web-app/js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let greetings = <h1>Hello World</h1>;
+
+ReactDOM.render(greetings, document.getElementById('root'));
