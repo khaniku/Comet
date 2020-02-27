ed9554b05bcabcc7cddcb45bcd39a2a90925c721
Nnamdi <nnamdinwchkw@yahoo.com>
2020-02-26 22:04:15 -0600

    Removed redundant folders/files

--- web-app/js/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-let greetings = <h1>Hello World</h1>;
-
-ReactDOM.render(greetings, document.getElementById('root'));
