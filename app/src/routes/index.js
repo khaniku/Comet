7d0c74ad8cdd133676b74fa7e686546e1dc428f4
Adrien <ajf029@shsu.edu>
2020-04-07 13:49:24 -0500

    removing some older code snippets

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -1,39 +1,39 @@
-<<<<<<< HEAD
 import React from "react";
 import { Switch } from "react-router-dom";
 import { Route, Redirect } from "react-router-dom";
 import Login from "../auth/login.js";
 import AuthChecker from "../auth/AuthChecker";
-import Dashboard from "../dashboard";
+// import Dashboard from '../dashboard';
+import Page from "../surveys/components/page";
 import SignUp from "../auth/signup.js";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
 class Routes extends React.Component {
-    render() {
-        return (
-        <Switch>
-            <Route path="/" exact component={AuthChecker} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/signup" exact component={SignUp} />
-            {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
-            <AuthenticatedRoute path="/dashboard" component={Dashboard} /> 
-        </Switch>
-        );
-    }
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={AuthChecker} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={SignUp} />
+        {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
+        <AuthenticatedRoute path="/surveys/components" component={Page} />
+      </Switch>
+    );
+  }
 }
 
 const AuthenticatedRoute = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
-    render={props =>
+    render={(props) =>
       localStorage.getItem("accessToken") ? (
         <Component {...props} />
       ) : (
         <Redirect
           to={{
             pathname: "/",
-            state: { from: props.location }
+            state: { from: props.location },
           }}
         />
       )
