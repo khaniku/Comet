929232e0b5469c71aa15a4fef3167d2d7de78aa8
ajacfabian <ajacfabian@gmail.com>
2020-04-09 09:25:25 -0500

    Committing things to be able to pull others

--- app/src/surveys/components/finalTable.jsx
+++ app/src/surveys/components/finalTable.jsx
@@ -14,7 +14,6 @@ import styling from '../../css/survey.css'
 
 import makeData from "./makeData";
 //import getData from "./getData";
-//import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 function DefaultColumnFilter({
   column: { filterValue, preFilteredRows, setFilter }
@@ -280,40 +279,13 @@ function App() {
     []
   );
 
-  function GetSurveys() {
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
-  
-    const auth = useSelector(state => state.auth);
-
-    //return 
-        //useEffect(() => {
-          return fetch('http://159.203.100.198:5000/api/survey/index', {
-		    method: 'GET',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-				Authorization: 'Bearer '+auth.accessToken
-		  	}
-		  }) 
-          .then((response) => response.json())
-          .then((json) => setData(json))
-          .catch((error) => console.error(error))
-          .finally(() => setLoading(false))
-        //})
-  }
-
-	GetSurveys().then(function(value){
-		console.log(value);
-	})
-
   const [data] = React.useState(() => makeData(10000));
-   React.useEffect(() => {}, [data]);
+
+  React.useEffect(() => {}, [data]);
 
   return (
     <div>
-		  {/*<Table columns={columns} data={GetSurveys} id="surveys" />*/}
-		  <Table columns={columns} data={data} id="surveys" />
+      <Table columns={columns} data={data} id="surveys" />
     </div>
   );
 }
